4d478e74f96a62a475a11ed4fb6246fd631578b3
Peyman Hassani <hassani.peyman627@gmail.com>
2021-02-27 19:33:02 +0330

    todo adding functionality

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from .models import Todo
 
 
 def index(request):
-    return render(request, 'index.html')
+    todos = Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(title=request.POST['title'])
+        new_todo.save()
+        return redirect('/')
+    return render(request, 'index.html', {'todos': todos})
